0d4ee102eb6274742cc0eb822883161e11a4b097
samuel atobatele <swumatob@yahoo.co.uk>
2021-01-21 16:30:54 +0100

    Created a controller for the R00lz framework

--- lib/r00lz.rb
+++ lib/r00lz.rb
@@ -6,7 +6,23 @@ module R00lz
 
   class App
     def call(env)
-      [200, {"content-type" => "text/html"}, ["Hello from R00lz!"]]
+      k1, act = cont_and_act(env)
+      text = k1.new(env).send(act)
+      [200, {"content-type" => "text/html"}, [text]]
+    end
+
+    def cont_and_act(env)
+      _, con, act, after = env["PATH_INFO"].split('/')
+      con = con.capitalize + "Controller"
+     [Object.const_get(con), act]
+    end
+  end
+
+  class Controller
+    attr_reader :env
+
+    def initialize(env)
+      @env = env
     end
   end
 end
